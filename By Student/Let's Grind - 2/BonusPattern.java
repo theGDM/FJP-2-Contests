c7476a5ebbfc0d564d4e274b7796607c0020e3e6
Gyandeep <gyandeepmehra370@gmail.com>
2021-11-23 09:23:53 +0530

    Modified bonus pattern

--- By Student/Let's Grind - 2/BonusPattern.java
+++ By Student/Let's Grind - 2/BonusPattern.java
@@ -25,28 +25,28 @@ public class BonusPattern {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int p =1;
-        int q = n*(n+1);
-        int u = q-n+1;
-        for(int i=1;i<=n;++i){
+        int p = 1;
+        int q = n * (n + 1);
+        int u = q - n + 1;
+        for (int i = 1; i <= n; ++i){
             //print -
-            for(int j=1;j<=2*(i-1);++j){
+            for (int j = 1; j <= 2 * (i - 1); ++j) {
                 System.out.print("-");
             }
-            for(int k =1;k<=n-(i-1);++k){
+            for (int k = 1; k <= n - (i - 1); ++k) {
                 System.out.print(p + "*");
                 ++p;
             }
             int o = u;
-            for(int l =1;l<=n-(i-1);++l){
-                if(l == n-(i-1)){
-                   System.out.print(o); 
-                }else{
-                   System.out.print(o + "*"); 
+            for (int l = 1; l <= n - (i - 1); ++l) {
+                if (l == n - (i - 1)) {
+                    System.out.print(o);
+                } else {
+                    System.out.print(o + "*");
                 }
                 ++o;
             }
-            u=u-(n-i);
+            u = u - (n - i);
             System.out.println();
             scn.close();
         }
