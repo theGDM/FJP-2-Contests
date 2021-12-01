6e042d3209efbe1525c1568e4345cb932e33ae80
Gyandeep <gyandeepmehra370@gmail.com>
2021-12-01 17:02:26 +0530

    Add camel case

---
+++ By Pepcoding/FJP-Contest-5/camelCase.java
@@ -0,0 +1,75 @@
+//Do run the code in the hackerrank ide if it is terminating without taking input
+
+// There is a sequence of words in CamelCase as a string of letters ,s, having the following properties:
+// It is a concatenation of one or more words consisting of English letters.
+// All letters in the first word are lowercase.
+// For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+// Given s, determine the number of words in s.
+
+// Example
+//oneTwoThree
+
+// There are  words in the string: 'one', 'Two', 'Three'.
+
+// Function Description
+// Complete the camelcase function in the editor below.
+
+// camelcase has the following parameter(s):
+// string s: the string to analyze
+
+// Returns
+// int: the number of words in 
+
+// Input Format
+// A single line containing string .
+
+// Constraints
+// 1 <= length of s <= 10^5
+
+// Sample Input
+// saveChangesInTheEditor
+
+// Sample Output
+// 5
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+    // Write your code here
+        int freqWord = 1;
+        for(int i = 0; i < s.length(); ++i){
+            if(i == 0 || s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
+                ++freqWord;
+            }
+        }
+        return freqWord;
+    }
+
+}
+
+public class camelCase {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
