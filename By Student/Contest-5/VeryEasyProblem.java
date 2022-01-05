e99655f4b005f51ce045abf5c1b1e57b21ed53e8
Gyandeep <gyandeepmehra370@gmail.com>
2022-01-05 10:15:03 +0530

    Add very easy problem

---
+++ By Student/Contest-5/VeryEasyProblem.java
@@ -0,0 +1,20 @@
+// There's no input.
+// Output some form of these numbers: 137, 1315, 73, 136, 255, 1384, 16385, one
+// per line in the listed order.
+// The first two lines of the CORRECT output file are:
+
+// 137=2(2(2)+2+2(0))+2(2+2(0))+2(0)
+// 1315=2(2(2+2(0))+2)+2(2(2+2(0)))+2(2(2)+2(0))+2+2(0)
+// Find the correct output for rest of them!.
+
+// Input Format
+// No Input
+
+// Constraints
+// No Constraints
+
+// Output Format
+// Same As It Is!
+
+// 137=2(2(2)+2+2(0))+2(2+2(0))+2(0)
+// 1315=2(2(2+2(0))+2)+2(2(2+2(0)))+2(2(2)+2(0))+2+2(0)
