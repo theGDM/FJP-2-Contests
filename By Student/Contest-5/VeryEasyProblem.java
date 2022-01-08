cfc8a366cc8058450184d95f28f2647f8ff9928b
Gyandeep <gyandeepmehra370@gmail.com>
2022-01-08 16:14:59 +0530

    Add simple problem

--- By Student/Contest-5/VeryEasyProblem.java
+++ By Student/Contest-5/VeryEasyProblem.java
@@ -18,3 +18,19 @@
 
 // 137=2(2(2)+2+2(0))+2(2+2(0))+2(0)
 // 1315=2(2(2+2(0))+2)+2(2(2+2(0)))+2(2(2)+2(0))+2+2(0)
+public class VeryEasyProblem {
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        System.out.println("137=2(2(2)+2+2(0))+2(2+2(0))+2(0)");
+        System.out.println("1315=2(2(2+2(0))+2)+2(2(2+2(0)))+2(2(2)+2(0))+2+2(0)");
+        System.out.println("73=2(2(2)+2)+2(2+2(0))+2(0)");
+        System.out.println("136=2(2(2)+2+2(0))+2(2+2(0))");
+        System.out.println("255=2(2(2)+2+2(0))+2(2(2)+2)+2(2(2)+2(0))+2(2(2))+2(2+2(0))+2(2)+2+2(0)");
+        System.out.println("1384=2(2(2+2(0))+2)+2(2(2+2(0)))+2(2(2)+2)+2(2(2)+2(0))+2(2+2(0))");
+        System.out.println("16385=2(2(2+2(0))+2(2)+2)+2(0)");
+    }
+}
